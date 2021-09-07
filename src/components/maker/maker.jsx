702d59604541b6957ea4a33df61a1f73a71841fa
DESKTOP-QFNES5C\MyCOM <mj.song@vogoplay.com>
2021-09-07 17:18:10 +0900

    add deitor and preview layout

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -3,6 +3,8 @@ import Header from "../header/header";
 import Footer from "../footer/footer";
 import styles from "./maker.module.css";
 import { useHistory } from "react-router";
+import Preview from "../preview/preview";
+import Editor from "../editor/editor";
 
 const Maker = ({ authService }) => {
   const history = useHistory();
@@ -21,6 +23,10 @@ const Maker = ({ authService }) => {
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor />
+        <Preview />
+      </div>
       <Footer />
     </section>
   );
