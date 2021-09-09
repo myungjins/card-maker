e2eb93e15b78aac1f097e2f9a8d2e609a408a35a
DESKTOP-QFNES5C\MyCOM <mj.song@vogoplay.com>
2021-09-10 01:03:02 +0900

    handle image uploading

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -6,7 +6,7 @@ import { useHistory } from "react-router";
 import Preview from "../preview/preview";
 import Editor from "../editor/editor";
 
-const Maker = ({ authService }) => {
+const Maker = ({ FileInput, authService }) => {
   const [cards, setCards] = useState({
     1: {
       id: "1",
@@ -77,6 +77,7 @@ const Maker = ({ authService }) => {
       <Header onLogout={onLogout} />
       <div className={styles.container}>
         <Editor
+          FileInput={FileInput}
           cards={cards}
           addCard={createOrUpdateCard}
           updateCard={createOrUpdateCard}
